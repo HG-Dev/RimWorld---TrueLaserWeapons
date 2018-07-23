45a55fffa3a5bf8306d13ea048375bd6765892a8
Ian M <ian.hg.dev@gmail.com>
2018-07-23 14:05:22 +0900

    [WIP] Upload to test Arasaka game asset courtesy of Shotty

---
+++ Source/Harmony/HarmonyStarter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using Verse;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+
+namespace Suffixware
+{
+	public static class HarmonyStarter
+	{
+        private static HarmonyInstance harmony = null;
+
+        static internal HarmonyInstance Instance
+        {
+            get
+            {
+                if (harmony == null)
+                    harmony = HarmonyInstance.Create("com.hg-dev.rimworld.mod.tlw");
+                return harmony;
+            }
+        }
+
+        public static void RunPatches()
+        {
+            // Remove the remark on the following to debug all auto patches.
+            HarmonyInstance.DEBUG = true;
+            Instance.PatchAll(Assembly.GetExecutingAssembly());
+            // Keep the following remarked to also debug manual patches.
+            HarmonyInstance.DEBUG = false;
+        }
+    }
+}
